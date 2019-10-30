7b46d6ac8e48fe7bf9092afdbe0aae5158a59cf9
Angela Vawser <amvawser@gmail.com>
2019-10-29 21:59:02 -0700

    build working but getting errors

---
+++ src/Character.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Character = (img, name, species) => (
+  <dl>
+    <dt>Image</dt>
+    <dd><img src={img}></img></dd>
+
+    <dt>Name</dt>
+    <dd>{name}</dd>
+
+    <dt>Species</dt>
+    <dd>{species}</dd>
+  </dl>
+);
+
+Character.propTypes = {
+  img: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  species: PropTypes.string.isRequired
+};
+
+export default Character;
